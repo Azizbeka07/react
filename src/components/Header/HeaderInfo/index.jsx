e7ea63ade0f10327deedc42fc4e070186add09ea
Azizbek <you@example.com>
2023-05-04 15:45:55 +0500

    components

---
+++ src/components/Header/HeaderInfo/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderInfo = () => {
+  return (
+    <div className="header-info">
+        <div className="container">
+            <p>HeaderInfo</p>
+        </div>
+    </div>
+  )
+};
+
+export default HeaderInfo;
---
+++ src/components/Header/HeaderNav/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderNav = () => {
+    return (
+      <div className="header-nav">
+          <div className="container">
+              <p>HeaderNav</p>
+          </div>
+      </div>
+    )
+  };
+  
+  export default HeaderNav;
