82706c3311552c6a701550ed35558cf47d607521
Azizbek <azizbekabdirimov2@gmail.com>
2023-05-12 16:23:55 +0500

    Calculator

--- src/About.jsx
+++ src/About.jsx
@@ -1,81 +1,105 @@
 import React, { useState } from "react";
+import Delete from "../src/assets/image/delete.png";
+import arrowDown from "../src/assets/image/arrow-down.png";
 
 const About = () => {
   const [count, setCount] = useState(0);
   const [firstCount, setFirstCount] = useState(0);
   const [result, setResult] = useState(0);
-  const [operator, setOperator] = useState("+")
+  const [operator, setOperator] = useState("+");
 
-  const addNumbers = () => {
-    return setResult(count * firstCount);
+  function addNumbers () {
+    switch (operator) {
+    case  "+":
+    setResult(count + firstCount)
+      break;
+     case "-":
+      setResult(count - firstCount)
+      break;
+     case "*":
+      setResult(count * firstCount);
+      break;
+     case "/":
+      setResult(count / firstCount)
   };
+  }
 
-  function reset () {
-    setCount(0)
-    setFirstCount(0)
-    setResult(0)
+  function reset() {
+    setCount(0);
+    setFirstCount(0);
+    setResult(0);
   }
   return (
-    <section className="background">
-      <div className="container">
-      <div className="contents fd-col mt-5">
-        <div className="increase-box">
-          <button className="color operator" onClick={() => setOperator("-")}>-</button>
-          <button className="color operator" onClick={() => setOperator("*")}>*</button>
-          <button className="color operator" onClick={() => setOperator("/")}>/</button>
-          <button className="color operator" onClick={() => setOperator("+")}>+</button>
-        </div>
-        <div className="count-box">
-          <h3 className="firstCount">{firstCount}</h3>
-          <button className="count-plus">{operator}</button>
-          <h3 className="count">{count}</h3>
-          <p className="count-plus">=</p>
-          <h3 className="result">{result}</h3>
-        </div>
+    <div className="container">
+      <div className="contents fd-col mt-5 background">
+          
+          <div className="count-box">
+            <h3 className="firstCount">{count}</h3>
+            <button className="count-plus">{operator}</button>
+            <h3 className="count">{firstCount}</h3>
+            <p className="count-plus">=</p>
+            <h3 className="result">{result}</h3>
+          </div>
         <div className="btn-box">
+        <div className="increase-box">
+            <button className="color operator" onClick={() => setOperator("-")}>
+              -
+            </button>
+            <button className="color operator" onClick={() => setOperator("*")}>
+              *
+            </button>
+            <button className="color operator" onClick={() => setOperator("/")}>
+              /
+            </button>
+            <button className="color operator" onClick={() => setOperator("+")}>
+              +
+            </button>
+          </div>
           <div className="btn-group">
-            <button
-              className="btn btn-green"
-              onClick={() => setCount(count + 1)}
-              disabled={count === 100}
-            >
-              Increment
+            <div className="arrow-box">
+              <button
+                className="btn btn-yellow"
+                onClick={() => setFirstCount(firstCount + 1)}
+                disabled={count === 100}
+              >
+                <img src={arrowDown} alt="img" className="arrowTop" />
+              </button>
+              <button
+                className="btn btn-yellow"
+                onClick={() => setFirstCount(firstCount - 1)}
+                disabled={firstCount === 0}
+              >
+                <img src={arrowDown} alt="img" className="arrow" />
+              </button>
+              <button></button>
+            </div>
+            <button className="btn btn-yellow" onClick={() => reset(0)}>
+              <img src={Delete} alt="img" className="delete" />
+            </button>
+          </div>
+          <div className="box-group">
+            <button className="btn btn-yellow" onClick={addNumbers}>
+              =
             </button>
             <button
-              className="btn btn-red"
+              className="btn btn-yellow"
+              onClick={() => setCount(count + 1)}
+              disabled={firstCount === 100}
+            >
+              <img src={arrowDown} alt="img" className="arrowTop" />
+            </button>
+            <button
+              className="btn btn-yellow"
               onClick={() => setCount(count - 1)}
               disabled={count === 0}
             >
-              Decrement
-            </button>
-            <button className="btn btn-yellow" onClick={() => reset(0)}>
-              Reset
-            </button>
-          </div>
-          <button className="btn btn-yellow" onClick={addNumbers}>
-            Result
-          </button>
-          <div className="btn-group">
-            <button
-              className="btn btn-green"
-              onClick={() => setFirstCount(firstCount + 1)}
-              disabled={firstCount === 100}
-            >
-              Increment
-            </button>
-            <button
-              className="btn btn-red"
-              onClick={() => setFirstCount(firstCount - 1)}
-              disabled={firstCount === 0}
-            >
-              Decrement
+              <img src={arrowDown} alt="img" className="arrow" />
             </button>
           </div>
         </div>
       </div>
       <div className="content"></div>
     </div>
-    </section>
   );
 };
 
